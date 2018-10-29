5c4cd4859602e087613669ae32431a241aa8ac7f
SSJ <SSJ@DESKTOP-2JN3BGK>
2018-10-29 03:26:11 +0200

    add face_recognition

--- facerecognition/facerecognition/face_recodnition.py
+++ facerecognition/facerecognition/face_recodnition.py
@@ -8,7 +8,7 @@ f.close()
 known_face_names, known_face_encodings = zip(*namedEncoding)
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
-    
+
     raise IOError("Cannot open webcam")
 while True:
 
@@ -26,11 +26,13 @@ while True:
             name = known_face_names[first_match_index]
 
         face_names.append(name)
+
     for face_location, name in zip(face_locations, face_names):
             top, right, bottom, left = face_location
             name_color = (0, 0, 255) if (name == "Unknown")  else (0, 255, 0)
             cv2.rectangle(frame, (left, top), (right, bottom), name_color, 2)
             cv2.putText(frame, name, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), lineType=cv2.LINE_AA)
+
     cv2.imshow('Face Cropper', frame)
     if cv2.waitKey(1) == 13:  # 13 is the Enter Key
         break
